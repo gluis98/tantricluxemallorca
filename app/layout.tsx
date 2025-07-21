48c6546d22c772b239709f634e51cb3a10acb3c6
kenny-lopez <kennylopezgallardo@gmail.com>
2025-07-21 09:29:43 -0400

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Urbanist } from 'next/font/google';
+import { Tenali_Ramakrishna } from 'next/font/google';
 import './globals.css';
 
 const urbanist = Urbanist({
@@ -7,10 +8,17 @@ const urbanist = Urbanist({
   display: 'swap',
 });
 
+const tenali = Tenali_Ramakrishna({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-tenali',
+  display: 'swap',
+});
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <body className={`${urbanist.variable} antialiased`}>
+      <body className={`${urbanist.variable} ${tenali.variable} antialiased`}>
         {children}
       </body>
     </html>
