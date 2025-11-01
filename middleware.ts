669facab9f9938a471172e621c4804a5b17d9629
kenny-lopez <kennylopezgallardo@gmail.com>
2025-11-01 15:32:07 -0400

    Fix sitemap and robots.txt for Netlify

--- middleware.ts
+++ middleware.ts
@@ -111,5 +111,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|images|.*\\..*).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|images|sitemap.xml|robots.txt|.*\\..*).*)'],
 };
